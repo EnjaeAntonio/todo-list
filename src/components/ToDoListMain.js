db9badef27a3facb59f566b9a749bec972749a67
Enjae Antonio <enjaeantonio@outlook.com>
2023-06-19 12:54:11 -0500

    filtering lists of tasks

--- src/components/ToDoListMain.js
+++ src/components/ToDoListMain.js
@@ -1,12 +1,26 @@
 import ToDoItem from "./ToDoItem";
 
 function ToDoListMain({todos, toggleTodo, deleteTodo}) {
+  const completedTodos = todos.filter(todo => todo.completed);
+  const unCompletedTodos = todos.filter(todo => !todo.completed);
 
   return (
     <>
     <h1 className="todo-heading text-4xl mt-3">Todo List</h1>
     <ul className="list container">
-    {todos.map(todo => {
+    {unCompletedTodos.map(todo => {
+      return (
+        <ToDoItem 
+        {...todo}
+        key={todo.id}
+        toggleTodo={toggleTodo}
+        deleteTodo={deleteTodo}
+        />
+      )
+    })}
+    </ul>
+    <ul className="list container">
+    {completedTodos.map(todo => {
       return (
         <ToDoItem 
         {...todo}
